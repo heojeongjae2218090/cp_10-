27da640bc90635f04516b7423894b9c78f6e1beb
heojeongjae2218090 <144421557+heojeongjae2218090@users.noreply.github.com>
2023-11-10 16:13:59 +0900

    Add files via upload

---
+++ cp_10 로또.py
@@ -0,0 +1,14 @@
+import random
+def generate_lotto_numbers():
+    lotto_numbers =[random.randint(1,45) for _ in range(6)]
+    lotto_numbers = list(set(lotto_numbers))
+    while len(lotto_numbers) < 6:
+        new_number = ramdom.randint(1, 45)
+        if new_number not in lotto_numbers:
+            lotto_numbers.append(new_number)
+
+    lotto_numbers.sort()
+    return lotto_numbers
+if __name__ == "__main__":
+    generate_numbers = generate_lotto_numbers()
+print("생성된 로또 번호:", generate_numbers)
