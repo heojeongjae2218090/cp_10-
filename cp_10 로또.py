19481534b28d869e46ff462ff4e1d782abb2a2be
heojeongjae2218090 <144421557+heojeongjae2218090@users.noreply.github.com>
2023-11-10 16:14:48 +0900

    Add files via upload

--- cp_10 로또.py
+++ cp_10 로또.py
@@ -3,7 +3,7 @@ def generate_lotto_numbers():
     lotto_numbers =[random.randint(1,45) for _ in range(6)]
     lotto_numbers = list(set(lotto_numbers))
     while len(lotto_numbers) < 6:
-        new_number = ramdom.randint(1, 45)
+        new_number = random.randint(1, 45)
         if new_number not in lotto_numbers:
             lotto_numbers.append(new_number)
 
